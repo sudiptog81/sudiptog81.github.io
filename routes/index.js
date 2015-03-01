2b6af7ba51c2a94776a5eec6369c87c8c85d76a6
Sudipto Ghosh <sudiptog81@outlook.com>
2015-03-01 14:10:21 +0530

    Included views

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+// var sendgrid  = require('sendgrid')(api_user, api_key);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,27 @@ router.get('/', function(req, res, next) {
    title: 'Sudipto Ghosh' ,
    email: 'mailto:sudiptog81@outlook.com' ,
    creator: 'Sudipto Ghosh',
-   git: '//cdn.rawgit.com/sudiptog81/sudiptoghosh/master/'
+   git: '//sudiptog81.github.io/'
+  });
+});
+
+/* GET clock */
+router.get('/clock/', function(req, res, next) {
+  res.render('clock', {
+   title: 'Clock' ,
+   email: 'mailto:sudiptog81@outlook.com' ,
+   creator: 'Sudipto Ghosh',
+   git: '//sudiptog81.github.io/'
+  });
+});
+
+/* GET rainbow */
+router.get('/rainbow/', function(req, res, next) {
+  res.render('rainbow', {
+   title: 'Rainbow' ,
+   email: 'mailto:sudiptog81@outlook.com' ,
+   creator: 'Sudipto Ghosh',
+   git: '//sudiptog81.github.io/'
   });
 });
 
